5f9bfbd92127c9508d6691773dd046411ff66e87
azizaTWDB <aziza.obryant@twdb.texas.gov>
2024-08-06 14:00:45 -0500

    setup

--- chicagoArt/collections.js
+++ chicagoArt/collections.js
@@ -1,1 +1,5 @@
 console.log('scripts have loaded...');
+
+//API criteia embeded in url link to slow load time for mass data... over 12k+ collections though
+var baseUrl = "https://api.artic.edu/api/v1/artworks?fields=id,image_id,title,artist_title,date_display,provenance_text,is_public_domain&limit=30"
+
