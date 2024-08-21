33b42b345e48fac67f278900100d3c6a91823e57
azizaTWDB <aziza.obryant@twdb.texas.gov>
2024-08-21 09:50:12 -0500

    dell 2

--- chicagoArt/collections.js
+++ chicagoArt/collections.js
@@ -1,5 +1,41 @@
-console.log('scripts have loaded...');
+
 
 //API criteia embeded in url link to slow load time for mass data... over 12k+ collections though
-var baseUrl = "https://api.artic.edu/api/v1/artworks?fields=id,image_id,title,artist_title,date_display,provenance_text,is_public_domain&limit=30"
+let url = "https://api.artic.edu/api/v1/artworks?fields=id,image_id,title,artist_title,date_display,provenance_text,is_public_domain&limit=40";
 
+fetch(url)
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    let col = data.data
+    let numb = Math.floor(Math.random() * col.length)
+    var apiImg = document.querySelector("#api-img");
+    var subhead = document.querySelector("#subhead");
+    var apiTitle = document.querySelector("#api-title");
+    var art = document.querySelector("#art");
+
+
+    col.filter()
+
+    function getArt(params) {
+      
+    }
+
+    function onTimer(a) {
+
+      
+    }
+
+  })
+
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  });
+
+  
