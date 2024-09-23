3c54e66dffb3eda43ae7db99e3a68be1b3a62891
Aziza123 <azizaobryant@gmail.com>
2024-09-23 13:38:07 -0500

    9/23

--- chicagoArt/test.js
+++ chicagoArt/test.js
@@ -3,7 +3,10 @@ let url = "https://api.artic.edu/api/v1/artworks?fields=id,image_id,title,artist
 var apiImg = document.querySelector("#api-img");
 var subhead = document.querySelector("#subhead");
 var apiTitle = document.querySelector("#api-title");
-var btn = document.querySelector(".bi");
+var art = document.querySelector("#art");
+var btn = document.querySelector(".bi-arrow-clockwise");
+
+
   
 
 
@@ -16,25 +19,30 @@ fetch(url)
   })
   .then(data => {
     let starter = data.data
-    console.log (starter[10])
+    console.log(starter[10].is_public_domain)
 
-    let newArr = starter.filter((artCol) => artCol.is_public_domain == true);
-
-    function changeImg() {
-
-      let x = Math.floor(Math.random() * newArr.length)
-      let art = newArr[x]
-      let newImg = "https://www.artic.edu/iiif/2/" + art.image_id + "/full/843,/0/default.jpg"
-
-      apiImg.setAttribute("src", newImg)
-
-    }
-
-    btn.addEventListener("click" , changeImg)
     
+    const newArr = starter.filter((artCol) => artCol.is_public_domain == true);
+
+    console.log(newArr)
+
+    function changeArt() {
+      let x = Math.floor(Math.random() * newArr.length)
+      var artCol = newArr[x];
+      console.log("Random collection chosen..." + artCol)
+
+      let imgSrc = "https://www.artic.edu/iiif/2/" + artCol.image_id + "/full/843,/0/default.jpg";
+      
+      apiImg.setAttribute("src", imgSrc)
+      apiTitle.textContent = artCol.title
+      subhead.innerText = "By: " + artCol.artist_title
+    }
+    
+    
+    btn.addEventListener("click" , changeArt)
+
   })
   .catch(error => {
     console.error('Error:', error);
   });
 
-//test
