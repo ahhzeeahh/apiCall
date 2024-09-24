897e18a07f57a5e09ee7017c21aa162f33494177
workAziza <azizatxgio@gmail.com>
2024-09-24 14:51:30 -0500

    full css

--- chicagoArt/collections.js
+++ chicagoArt/collections.js
@@ -1,48 +1,49 @@
 
-
-//API criteia embeded in url link to slow load time for mass data... over 12k+ collections though
-let url = "https://api.artic.edu/api/v1/artworks?fields=id,image_id,title,artist_title,date_display,provenance_text,is_public_domain&limit=60";
-
-var apiImg = document.querySelector("#api-img");
-var subhead = document.querySelector("#subhead");
-var apiTitle = document.querySelector("#api-title");
-var art = document.querySelector("#art");
+let url =
+  "https://api.artic.edu/api/v1/artworks?fields=id,image_id,title,artist_title,date_display,provenance_text,is_public_domain&limit=40";
+let imageInterval; // To store the interval ID
 
 fetch(url)
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error("Network response was not ok");
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Something went wrong on API server!");
     }
-    return res.json();
   })
+  .then((data) => {
+    const col = data.data;
+    var apiImg = document.querySelector("#api-img");
+    var subhead = document.querySelector("#subhead");
+    var apiTitle = document.querySelector("#api-title");
+    var art = document.querySelector("#art");
+    var btn = document.querySelector(".bi");
 
-  .then((img) => {
-  
-    fetch(url).then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Something went wrong on API server!");
-      }
-    })
-    
-    .then((img) => {
-        let x = img.data
-        let newArr = x.filter(item => item.is_public_domain == true)
-        var numb = Math.floor(Math.random() * newArr.length);
-   
+    let x = col.filter((post) => post.is_public_domain && post.image_id != null);
 
-        function changeItem() {
+    function changeImage() {
+      let randomIndex = Math.floor(Math.random() * x.length);
+      let post = x[randomIndex];
 
-          apiImg.toggle() = "show"
-        }
+      let aSrc ="https://www.artic.edu/artworks/" + post.id
+      let imgSrc =
+        "https://www.artic.edu/iiif/2/" + post.image_id + "/full/843,/0/default.jpg";
 
+      apiImg.setAttribute("src", imgSrc);
+      art.setAttribute("href", aSrc);
 
-        //document.querySelector("#btn").addEventListner("click", changeItem())
-    })
-    
-    .catch((error) => {
-      console.error(error);
-    });
+      subhead.textContent = "By: " + post.artist_title;
 
+      apiTitle.textContent = `"` + post.title + `"`
+
+      console.log("Col updated.");
+    }
+
+    btn.addEventListener("click", changeImage);
+
+    changeImage(); // Initial image
+    imageInterval = setInterval(changeImage, 10000); // Change image every 10 seconds
   })
+  .catch((error) => {
+    console.error(error);
+  });
