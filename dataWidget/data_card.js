04a74105ddf881ae638d68cdbe22091a65e02fbb
azizaTWDB <aziza.obryant@twdb.texas.gov>
2024-12-17 15:40:48 -0600

    folder update

--- dataWidget/data_card.js
+++ dataWidget/data_card.js
@@ -1,5 +1,5 @@
 
-let url = "https://api.tnris.org/api/v1/collections";
+let url = "https://api.tnris.org/api/v1/collections_catalog?limit=5";
 
 fetch(url)
 
@@ -15,9 +15,24 @@ fetch(url)
 .then((data) => {
   let d = data.results
   let previewImg = d[0].thumbnail_image 
+  let slider = document.querySelector("#js-img-insert");
   document.querySelector("#dataImg").setAttribute("src", previewImg)
   document.querySelector("#dataCardTitle").setAttribute("src", previewImg)
   console.log(previewImg)
+
+  d.forEach(e => {
+    
+    let dataCol = document.createElement("div");
+    dataCol.classList("carousel-item")
+    let dataImg = document.createElement("img");
+
+    e.thumbnail_image.setAttribute("src", dataImg);
+    dataImg.classList("d-block w-100")
+
+
+
+    slider.appendChild(dataCol);
+  });
   
  
 })
