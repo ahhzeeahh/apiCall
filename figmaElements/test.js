e3001aed243adbe8ae0c9176be2119a9548843a4
workAziza <azizatxgio@gmail.com>
2025-09-23 10:10:40 -0500

    start1

--- figmaElements/test.js
+++ figmaElements/test.js
@@ -1,10 +1,36 @@
-console.log('test')
+
+var sectionInsert = document.getElementById("insertContracts")
+function setDisplayNone() {
+    let p = document.getElementById("status-message")
+    p.innerHTML = "No contracts available at the moment..."
+}
+
+
+
 fetch("https://test.txgio.org/contracts/index.json")
-    .then(response => response.json())
 
-    .then(data => console.log(data)
+    .then((response) => {
+      if (response.ok == true) {
+        return response.json()
 
+      } else {
+        setDisplayNone();
+        console.log("hi")
+         }
+})
+    .then(data => {
+       
+        let all = data.results //change this to .all later!!!!!!!!!!!!!!!
+
+         console.log(all);
+
+       
+
+         
+}
 )
-    .catch(error => console.error('Error:', error)
-
-);
+    .catch(error => {
+      
+        setDisplayNone();
+        console.log("rip...ERROR =" + error)
+});
