cf5ed300158e5f73d12c49f4a7f052a0135321f4
workAziza <azizatxgio@gmail.com>
2025-09-24 11:37:01 -0500

    push5

--- figmaElements/test.js
+++ figmaElements/test.js
@@ -2,10 +2,13 @@
 let sectionInsert = document.getElementById("insertContracts")
 let p = document.getElementById("status-message")
 let abcNav = document.getElementById("alpha-nav")
-let currArr = ""
 
 function setDisplayNone() {
-    p.innerHTML = "No contracts available at the moment..."
+      setTimeout(() =>{
+         p.textContent = "No contracts available at the moment..."
+            }, 500);
+        p.innerHTML=  `<img alt="loading image" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif">`
+       
 }
 
 fetch("https://test.txgio.org/contracts/index.json")
@@ -20,38 +23,37 @@ fetch("https://test.txgio.org/contracts/index.json")
          }
 })
     .then(data => {
-       
+       p.innerHTML=  `<img alt="loading image" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif">`;
        let allContracts = data.providers 
-       console.log(allContracts)
+
 
         function getArrayFiltered(e) {
+            console.log("i clicked " + e.target.innerText)
 
-            let afterArr = allContracts.filter(beforeArr => e.target.innerText == beforeArr.Tab);
-            console.log(afterArr)
-            if (afterArr.length = 0) {
-                setDisplayNone()    
+            let afterArr = allContracts.filter(beforeArr => beforeArr.Tab === e.target.innerText);
+            if (afterArr.length == 0) {
+                sectionInsert.innerHTML = ""
+                setDisplayNone()
             }else{
-            afterArr = currArr;
-            console.log(currArr)
-            makeBlocks(currArr)
+                makeBlocks(afterArr)
             }
         }
             
 
 
 
-       function makeBlocks(currArr) {
+       function makeBlocks(currentArr) {
                 sectionInsert.innerHTML = "" // clear before each round
                 p.innerHTML = "";
-                    currArr.forEach(e => {
-                        console.log(e.Tab == "A")
+                    currentArr.forEach(e => {
+                        
                         let divHolder = document.createElement("div");
                         divHolder.className = "contract-box"
 
                         divHolder.innerHTML = `
-                                <img height="60px" alt="${e.Title} logo and web links" class="d-block w-100" src="${e.Logo}">
+                                <img height="100px" alt="${e.Title} logo and web links" class="d-block w-100" src="${e.Logo}">
                                 <h5 class="mt-3 text-dark fw-bold">${e.Title}</h5>
-                               <a  href="${e.website}" target= "_blank">Contact Page</a><br>
+                               <a  href="${e.Website}" target= "_blank">Contact Page</a><br>
                                 <a  href="${e.DIRlink}" target= "_blank">DIR Contact Page</a>
                     
                         `;   
